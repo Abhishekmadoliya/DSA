c03ff0685d3f557e3dc0cfe6f12a5df47bfcfde1
Abhishek madoliya <abhishekmadoliya@gmail.com>
2025-02-22 19:16:36 +0530

    working on 3 sum approach hashing using map ds

--- hashing/3sum.js
+++ hashing/3sum.js
@@ -1,23 +1,32 @@
 
-// ----------------BRUTE -- FORCE------------------------
-function _3sum(array) {
-    let result =new Set([]);
+// // ----------------BRUTE -- FORCE------------------------
+// function _3sum(array) {
+//     let result =new Set([]);
 
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i+1; j < array.length; j++) {
-           for (let k = j+1; k < array.length; k++) {
-            if (array[i]+array[j]+array[k] == 0) {
-                let newresult =[ array[i],array[j],array[k]]
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i+1; j < array.length; j++) {
+//            for (let k = j+1; k < array.length; k++) {
+//             if (array[i]+array[j]+array[k] == 0) {
+//                 let newresult =[ array[i],array[j],array[k]]
 
-                newresult.sort();
-                result.add(newresult.toString());
-            }
-           }
+//                 newresult.sort();
+//                 result.add(newresult.toString());
+//             }
+//            }
             
-        }
-    }
+//         }
+//     }
     
-    return result
+//     return result
+// }
+
+// console.log(_3sum([-1,0,1,2,-1,-4]));
+
+
+// more optimal approach using hashing ----maps
+
+function _3sum(arr) {
+    
 }
 
-console.log(_3sum([-1,0,1,2,-1,-4]));
+
