3a7a1b1bf0f6ec8782fb6ea263362e101cb7f3bc
Abhishek madoliya <abhishekmadoliya@gmail.com>
2025-02-25 16:33:47 +0530

    solving 4sum using two pointer approach

--- hashing/4sum.js
+++ hashing/4sum.js
@@ -1,9 +1,26 @@
 
-function _4sum(params) {
+function _4sum(array, target) {
     
 
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+    array.sort((a,b)=> a-b);
+
+    for (let i = 0; index < array.length; i++) {
+        for (let j = i+1; j < array.length; j++) {
+            let p = array[j+1];
+            let q = array[array.length-1];
+
+            while (p<q) {
+                
+                if (array[i]+array[j]+p+q < target) {
+                    p++;
+                }else if (array[i]+array[j]+p+q > target) {
+                    q++;
+                }else{
+                     
+                }
+            }
+            
+        }
         
     }
 }
