7a47f2c33c94485c9efa2ccd00d8fcdf47b7acd0
Abhishek madoliya <abhishekmadoliya@gmail.com>
2025-02-25 16:54:47 +0530

    4sum solved using 2 pointer approach

--- hashing/4sum.js
+++ hashing/4sum.js
@@ -1,26 +1,44 @@
 
 function _4sum(array, target) {
     
-
+    let ans =[];
     array.sort((a,b)=> a-b);
 
-    for (let i = 0; index < array.length; i++) {
-        for (let j = i+1; j < array.length; j++) {
-            let p = array[j+1];
-            let q = array[array.length-1];
+    for (let i = 0; i < array.length; i++) {
+        if (i>0 && array[i] == array[i-1]){
+            continue;
+        }
+        for (let j = i+1; j < array.length; ) {
+            // if (j>i+1 && array[j] == array[j-1]){
+            //     continue;
+            // }
+            let p = j+1;
+            let q = array.length-1;
 
             while (p<q) {
+                let sum = array[i]+array[j]+array[p]+array[q];
                 
-                if (array[i]+array[j]+p+q < target) {
+                if (sum < target) {
                     p++;
-                }else if (array[i]+array[j]+p+q > target) {
-                    q++;
+                }else if (sum > target) {
+                    q--;
                 }else{
-                     
+                     ans.push([array[i],array[j],array[p],array[q] ]);
+                     p++;
+                     q--;
+                     while (p<q && array[p] == array[p-1]) {
+                        p++;
+                     }
                 }
             }
-            
+            j++;
+            while (j< array.length && array[j] == array[j-1]) {
+                j++;
+            }
         }
         
+
+        return ans;
     }
 }
+console.log( _4sum([1,2,3,-4,-6]));
